a3cc994f9fca9273ee905ecfe2d2dce06f1daad7
sandy99135 <sandiarivelo.rakotonindrina@accesbanque.com>
2021-11-16 09:45:54 +0300

    modif

--- dist/app.js
+++ dist/app.js
@@ -5,7 +5,8 @@ const route_resolver_1 = require("./core/resolvers/route-resolver");
 const request_obsever_resolver_1 = require("./core/resolvers/request-obsever-resolver");
 const request_observer_config_1 = require("./app-start/request-observer-config");
 const route_config_1 = require("./app-start/route-config");
-let server = new server_1.HttpServer(3000);
+require('dotenv').config();
+let server = new server_1.HttpServer(3000 || Number(process.env.PORT));
 server.start();
 let routeResolver = route_resolver_1.RouteResolver.getInstance(server);
 route_config_1.RouteConfig.register(routeResolver);
@@ -13,12 +14,4 @@ server.registerRoutes();
 // observer
 let observerResolver = request_obsever_resolver_1.RequestObserverResolver.getInstance(server);
 request_observer_config_1.RequestObserverConfig.registerObsever(observerResolver);
-// const ViberBot = require('viber-bot').Bot;
-// const bot : any = new ViberBot({
-//     authToken: AppConfig.VIBER_TOKEN,
-//     name: AppConfig.VIBER_BOT_NAME,
-//     avatar: "https://upload.wikimedia.org/wikipedia/commons/3/3d/Katze_weiss.png"
-// });
-// // console.log(bot);
-console.log();
 //# sourceMappingURL=app.js.map
--- src/core/interfaces/base-controller.ts
+++ src/core/interfaces/base-controller.ts
@@ -1,13 +1,10 @@
 import { HttpRequestModel } from "../models/http-request.model";
 import { HttpResponseModel } from "../models/http-response.model";
-import { TranslationService } from "../../translations/translation-service";
 
 
 export abstract class BaseController {
     public request: HttpRequestModel;
     public response: HttpResponseModel|any;
-    protected transService: TranslationService;
-    
     constructor(){
         this.request = new HttpRequestModel();
         this.response = new HttpResponseModel();
